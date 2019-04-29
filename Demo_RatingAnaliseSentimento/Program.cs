6352ae6e385747059e5b943b8b71805dde77048a
Cleiton Felipe de Moraes <omehe.cleiton@hotmail.com>
2019-04-28 22:18:39 -0300

    Add project files.

---
+++ Demo_RatingAnaliseSentimento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_RatingAnaliseSentimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0
+            Console.WriteLine("Entre com o texto positivo ou negativo");
+            var texto = Console.Read();
+            var sentimentServices = SentimentService.GetSentiment(texto.ToString());
+            Console.WriteLine(sentimentServices.Result);
+
+            //Console.WriteLine("Entre com o texto negativo");
+            //var textoNegativo = Console.Read();
+            //var sentimentServicesNegativo = SentimentService.GetSentiment(textoNegativo.ToString());
+            //Console.WriteLine(sentimentServicesNegativo.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Demo_RatingAnaliseSentimento/SentimentService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo_RatingAnaliseSentimento
+{
+    public static class SentimentService
+    {
+        //Example Base Url: https://westus.api.cognitive.microsoft.com
+        const string _sentimentAPIBaseUrl = "<Base URL API>";
+        const string _textSentimentAPIKey = "<API Key>";
+        //HttpClient wrapper for accessing the TextAnalytics API
+        readonly static TextAnalyticsClient _textAnalyticsApiClient 
+            = new TextAnalyticsClient(new ApiKeyServiceClientCredentials(_textSentimentAPIKey))
+        {
+            Endpoint = _sentimentAPIBaseUrl
+        };
+        //Input the text to be analyzed and return its sentiment score. 
+        //The sentiment score will range from 0 to 1, where 0 is negative sentiment and 1 is positive sentiment.
+        public static async Task<double?> GetSentiment(string text)
+        {
+            //Create the request object to send to the TextAnalytics API
+            //The request can contain multiple text inputs which you can use to batch multiple requests into one API call
+            var request = new MultiLanguageBatchInput(new List<MultiLanguageInput>
+            {
+                { new MultiLanguageInput(id: "1", text: text) }
+            });
+            //Get the sentiment results from the TextAnalytics API 
+            var sentimentResult = await _textAnalyticsApiClient.SentimentAsync(request);
+            //Parse the sentiment score 
+            return sentimentResult?.Documents?.FirstOrDefault()?.Score;
+        }
+        //Helper class to add our API Key to the HttpRequestMessage Header
+        class ApiKeyServiceClientCredentials : ServiceClientCredentials
+        {
+            readonly string _subscriptionKey;
+            public ApiKeyServiceClientCredentials(string subscriptionKey)
+            {
+                _subscriptionKey = subscriptionKey;
+            }
+            public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if (request is null)
+                    throw new ArgumentNullException(nameof(request));
+                request.Headers.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
